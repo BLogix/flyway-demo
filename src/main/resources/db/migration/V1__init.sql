b006fed5b3fe75892cada323babd27bb30e0447f
Branden Boyington <branden.boyington@eagles.usm.edu>
2020-01-05 10:25:52 -0600

    changed init table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,20 @@
-DROP TABLE IF EXISTS billionaires;
-
-CREATE TABLE billionaires
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name  VARCHAR(250) NOT NULL,
-    career     VARCHAR(250) DEFAULT NULL
+create table personal_best_apps_and_games_of_2019(
+    id  int auto_increment primary key,
+    name varchar(40),
+    maker varchar(40),
+    rating int
 );
 
-INSERT INTO billionaires (first_name, last_name, career)
-VALUES ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-       ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-       ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+insert into personal_best_apps_and_games_of_2019
+    (name, maker)
+values
+    ('TikTok', 'TikTok Inc.'),
+    ('Maki', 'Sunshine Apps'),
+    ('Discord', 'Discord Inc.'),
+    ('Teams', 'Microsoft Corporation'),
+    ('Hearthstone', 'Blizzard Entertainment, Inc.'),
+    ('Bible', 'Life.Church'),
+    ('Brave Browser', 'Brave Software'),
+    ('Meetup', 'Meetup'),
+    ('Acorns', 'Acorns'),
+    ('Nextdoor', 'Nextdoor.com');
